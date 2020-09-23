04f28c60929693399c293cace983be2040b1410e
yoyofbsi <yoyo.frey@free.fr>
2020-09-23 09:27:20 +0200

    ajoutFonctionCheckWinnersLosers

---
+++ checkWinnersLosers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (int soldeA, int soldeB){
+	if (soldeA > objectifA){
+		printf("Joueur A vous avez depasse l'objectif que vous vous ete fixe")
+	}
+	else if (soldeB > objectifB){
+		printf("Joueur B vous avez depasse l'objectif que vous vous ete fixe")
+	}
+	else if (){
+		
+	}
+	return 0;
+}
